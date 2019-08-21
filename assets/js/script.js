b8ba52bb3ee009a28b86a4709edf2fd102ed538e
Adison Lay <adisonlay@gmail.com>
2019-08-20 23:26:48 -0700

    added functionality to flip one card, fixed card positioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+$(document).ready(initializeApp);
 
+function initializeApp() {
+  $('.card').click(handleCardClick);
+}
+
+function handleCardClick(event) {
+  console.log(event.currentTarget);
+  $(event.currentTarget).toggleClass('hidden');
+}
